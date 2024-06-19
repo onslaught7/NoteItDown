6014a70bd36dadcb071c0f7b9f2b1cc0400cb9b0
onslaught7 <krutibashmohapatra7@gmail.com>
2024-06-19 13:53:16 +0530

    Api application integrated using AXIOS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg"
+import pg from "pg";
 import axios from "axios";
 
 const app = express();
@@ -21,6 +21,27 @@ db.connect();
 
 let books = [];
 
+async function fetchBookImage(bookId, bookTitle) {
+    try {
+        const response = await axios.get(
+            `https://openlibrary.org/search.json?title=${encodeURIComponent(bookTitle)}`
+        );
+
+        if (response.data.docs && response.data.docs.length > 0) {
+            const coverId = response.data.docs[0].cover_i;
+            let imageUrl = '';
+
+            if (coverId) {
+                imageUrl = `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`; // Using 'L' for large cover size
+            }
+
+            // Update the database with the fetched image URL
+            await db.query("UPDATE books_list SET imageurl = $1 WHERE id = $2", [imageUrl, bookId]);
+        } 
+    } catch (err) {
+        console.error(`Failed to fetch image for book '${bookTitle}':`, error);
+    }
+}
 app.get("/", async (req, res) => {
     try {
         const result = await db.query(
@@ -28,6 +49,18 @@ app.get("/", async (req, res) => {
         ); 
     
         books = result.rows;
+
+        for(let book of books) {
+            if (!book.imageurl) {
+                await fetchBookImage(book.id, book.title);
+            }
+        }
+
+        const updatedResult = await db.query(
+            "SELECT books_list.id, books_list.title, imageurl, rating, notes FROM books_list JOIN book_notes ON books_list.id = book_notes.book_id ORDER BY books_list.id ASC;"
+        );
+
+        books = updatedResult.rows;
 
         res.render("index.ejs", {
             books: books,
@@ -38,23 +71,32 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/book", async (req, res) => {
-    try{
+    try {
         const bookId = req.body.id;
         const result = await db.query(
-            "SELECT books_list.title, imageurl, rating, summary, notes FROM books_list JOIN book_notes ON books_list.id = book_notes.book_id WHERE books_list.id = ($1) ORDER BY books_list.id ASC;",
+            "SELECT books_list.title, imageurl, rating, summary, notes FROM books_list JOIN book_notes ON books_list.id = book_notes.book_id WHERE books_list.id = $1 ORDER BY books_list.id ASC;",
             [bookId]
         );
 
-        // always specify the index position since it returns an array of Object
-        // therefore we have to specify the index of the array to be sent over
-        // In this case we send the first object containing the details from our query stored in result in our array
-        // This will always be 0 since only one object will be returned from each run of the query, one book is a unique id and corresponds to only one record
-        const book = result.rows[0];
+        let book = result.rows[0];
+
+        if(!book.imageurl) {
+            await fetchBookImage(bookId, book.title);
+
+            const updatedResult = await db.query(
+                "SELECT books_list.title, imageurl, rating, summary, notes FROM books_list JOIN book_notes ON books_list.id = book_notes.book_id WHERE books_list.id = $1 ORDER BY books_list.id ASC;"
+            );
+            // always specify the index position since it returns an array of Object
+            // therefore we have to specify the index of the array to be sent over
+            // In this case we send the first object containing the details from our query stored in result in our array
+            // This will always be 0 since only one object will be returned from each run of the query, one book is a unique id and corresponds to only one record
+            book = updatedResult.rows[0];
+        }
 
         res.render("book.ejs", {
             book: book
         });        
-    } catch (err){
+    } catch (err) {
         console.log(err);
     }
 });
